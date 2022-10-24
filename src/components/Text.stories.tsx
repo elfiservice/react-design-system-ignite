174a90d0f0c3a519fad7042381cf7d9744d5a47f
Ant ARmando Junior <aaraujo@descartes.com>
2022-10-24 19:53:28 -0300

    feat : provided Text component variants in storybook

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,26 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
 
 export default {
   title: 'Components/Text',
   component: Text,
+  args: {
+    children: 'Lorem ipsum.',
+  },
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    children: 'Lorem ipsum.',
+    size: 'sm'
+  },
 }
 
-export const Default = {}
+export const Large: StoryObj<TextProps> = {
+  args: {
+    children: 'Lorem ipsum.',
+    size: 'lg'
+  },
+}
